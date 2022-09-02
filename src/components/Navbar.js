2f4762ebd41ff9a692d7763d4fbface52b5646b2
Rahul <rahulchalat@yahoo.com>
2022-09-02 22:09:06 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Cascading-Style-Sheets/Navbar.css";
+import { BsSearch } from "react-icons/bs";
+import wallet from "../assets/wallet.png";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="nav">
+        <input type="search" name="" id="" placeholder="Search" />
+        <BsSearch className="search-img" />
+        <button>Sign Up</button>
+        <img src={wallet} height={45} width={45} alt="" />
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
